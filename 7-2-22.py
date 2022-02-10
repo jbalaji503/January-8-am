8604077c6269b76b062fa7421ef93d7627e19ad9
jbalaji503 <98318302+jbalaji503@users.noreply.github.com>
2022-02-10 09:46:21 +0530

    Update 7-2-22.py

--- 7-2-22.py
+++ 7-2-22.py
@@ -1,20 +1,30 @@
-'''li1=[2,4,3,6,5]
+#Create an empty list (two ways)
+
+li1=[2,4,3,6,5]
 li2=[]
 li1.clear()
 print(li1)
-print(li2)'''
+print(li2)
 
-'''li1=[8,9,1,5,6,7,8,-1]
+#add 8,9,1,5,6,7,8,1 elements to that list
+#Find frequency of 8 (count)
+#find the mean of the list
+
+li1=[8,9,1,5,6,7,8,-1]
 print(li1.count(8))
 li1.sort()
 print(li1)
 li1.reverse()
-print(li1)'''
+print(li1)
 
-'''li1=[8,9,1,5,6,7,]
+#find sum (List) + min + Max 
+
+li1=[8,9,1,5,6,7,]
 print(sum(li1))
 print(min(li1))
-print(max(li1))'''
+print(max(li1))
+
+#remove duplicates from list and give output in the format of tuple
 
 li1={8,9,1,5,6,7,8,8,2}
 print(li1)
